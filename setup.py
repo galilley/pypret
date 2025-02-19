6fdd0ce596a770e0aea4a18dc53cdf4cb9d2dd7e
Denis Sergeevich <galilley@gmail.com>
2025-02-19 19:47:07 +0700

    add setup.py to use with git install

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+MIT License
+
+Copyright (c) 2019 Nils C. Geib
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+metadata = dict(
+    name="pypret",
+    version="1.0",
+    license="MIT",
+    platforms="POSIX",
+    author="Nils C. Geib",
+    author_email="",
+    description=("Python for ultrashort laser pulse retrieval "),
+    keywords=["photonic", "ultra-short pulses", "field"],
+    packages=["pypret", "doc", "scripts"],
+    long_description=open("README.md").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics"
+    ]
+)
+
+if __name__ == "__main__":
+
+    try:
+        from setuptools import setup
+
+    except ImportError:
+        from distutils.core import setup
+
+    # No matter which imports were used, use same setup call:
+    setup(**metadata)
